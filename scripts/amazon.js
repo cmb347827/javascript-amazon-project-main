e756fb99e6768a7eb89128398fbc47d17d49c952
cmb347827 <v38d422ll621dem@gmail.com>
2024-02-23 17:51:35 -0500

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,20 +25,20 @@ products.forEach((product)=>{
           $${(product.priceCents /100).toFixed(2)}
         </div>
 
-        <div class="product-quantity-container">
-          <select>
+        <div>
+          <select id='item-quantity'>
             <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
+            <option  value="2">2</option>
+            <option  value="3">3</option>
+            <option  value="4">4</option>
+            <option  value="5">5</option>
+            <option  value="6">6</option>
+            <option  value="7">7</option>
+            <option  value="8">8</option>
+            <option  value="9">9</option>
+            <option  value="10">10</option>
           </select>
-        </div>
+        </div> 
 
         <div class="product-spacer"></div>
 
@@ -60,24 +60,34 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
    button.addEventListener('click',()=>{
       const productId= button.dataset.productId;
       let matchingItem;
+      let cartQuantity=0;
       //loop to see if item is already in cart
       cart.forEach((item)=>{
+        console.log('cart isn\'t empty and item is in cart');
         if(productId===item.productId){
            matchingItem=item;
         }
       });
+      
       if(matchingItem){
+        console.log('as is visible here');
         //item.quantity+=1 below line 66 increases the quantity for each individually added same item.
         //item is an object, so copied to matchingItem, matchingItem is an object, can add properties:quantity.
         matchingItem.quantity+=1;
+
       }else{
         //item is not yet in cart.
+        console.log('item is not yet in cart');
         cart.push({
           productId,
-          quantity:1
+          quantity: 1,
         });
       }
       
-      console.log(cart);
+      cart.forEach((item)=>{
+         cartQuantity+=item.quantity;
+      });
+      
+      document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
    });
 });
